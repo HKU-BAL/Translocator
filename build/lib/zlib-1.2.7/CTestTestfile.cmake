2916ba51da4a62d64a5b25a899077ec83462d28f
ywu <ywu@bal32.cs.hku.hk>
2020-01-21 21:58:38 +0800

    first commit

---
+++ build/lib/zlib-1.2.7/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /nas2/ywu/Translocator/lib/zlib-1.2.7
+# Build directory: /nas2/ywu/Translocator/build/lib/zlib-1.2.7
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
